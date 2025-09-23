edb74af47a7650a2608140ceb81ed37f0cc297e9
rafaelcarlosoficial <143357132+rafaelcarlosoficial@users.noreply.github.com>
2025-09-23 17:26:43 -0300

    Tailwind configurado com sucesso

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>The footer is here I wish you so much fun! </p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ app/components/header/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <div className="navbar flex justify-between bg-orange-600">
+          <p>teste</p>
+          <p>teste</p>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
