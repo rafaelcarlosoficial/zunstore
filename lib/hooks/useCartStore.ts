34502795e763cd77f2dd1ca7246edffc98703997
rafaelcarlosoficial <143357132+rafaelcarlosoficial@users.noreply.github.com>
2025-10-14 10:33:16 -0300

    increase e decrease funcionando normalmente

--- lib/hooks/useCartStore.ts
+++ lib/hooks/useCartStore.ts
@@ -18,7 +18,6 @@ const initialState: Cart = {
   shippingPrice: 0,
   totalPrice: 0,
 }
-
 export const cartStore = create<Cart>()(
   persist(() => initialState, {
     name: 'cartStore',
@@ -73,11 +72,21 @@ export default function useCartStore() {
     decrease: (item: OrderItem) => {
       //check if the elemment exist in the items array
       const exist = items.find((x) => x.slug === item.slug)
+      //if is undefined ou null will return
       if (!exist) return
+
       const updatedCartItems =
         exist.qty === 1
-          ? items.filter((x: OrderItem) => x.slug !== item.slug)
-          : items.map((x) => (item.slug ? { ...exist, qty: exist.qty - 1 } : x))
+          ? // It's actually taking all array elements that are different from x and making a new array
+            items.filter((x: OrderItem) => x.slug !== item.slug)
+          : //Probably the error is here, map is taking all the elements and subtracting everything
+
+            // items.map((x) => (item.slug ? { ...exist, qty: exist.qty - 1 } : x))
+            items.map((x) =>
+              x.slug === item.slug ? { ...x, qty: x.qty - 1 } : x
+            )
+
+      //This one is made to calculate the price.. because its need to be updated.
       const { itemsPrice, shippingPrice, taxPrice, totalPrice } =
         calcPrice(updatedCartItems)
       cartStore.setState({
