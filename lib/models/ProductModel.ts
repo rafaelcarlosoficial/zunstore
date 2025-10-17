27f0f8ad337cd58ebf01411079906d33c3429625
rafaelcarlosoficial <143357132+rafaelcarlosoficial@users.noreply.github.com>
2025-10-17 16:57:07 -0300

    Conectando com database

--- lib/models/ProductModel.ts
+++ lib/models/ProductModel.ts
@@ -1,3 +1,34 @@
+import mongoose from 'mongoose'
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    image: { type: String, required: true },
+    image2: { type: String, required: true },
+    image3: { type: String, required: true },
+    image4: { type: String, required: true },
+    discount: { type: Number, required: true, default: 0 },
+    banner: { type: String },
+    price: { type: Number, required: true },
+    installments: { type: Number, required: true },
+    brand: { type: String, required: true },
+    category: { type: String }, // optional
+    rating: { type: Number, required: true, default: 0 },
+    numReviews: { type: Number, required: true, default: 0 },
+    countInStock: { type: Number, required: true, default: 0 },
+  },
+  {
+    timestamps: true, // adds createdAt and updatedAt automatically
+  }
+)
+
+// Prevents model overwrite error during hot reloads
+const ProductModel =
+  mongoose.models.Product || mongoose.model('Product', productSchema)
+
+export default ProductModel
+
 export type Product = {
   _id?: string
   name: string
