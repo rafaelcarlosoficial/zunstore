de0e8cd52e573e9351d60eb199c4b5bfc94b2530
rafaelcarlosoficial <143357132+rafaelcarlosoficial@users.noreply.github.com>
2025-12-04 19:16:23 -0300

    Resolvendo bug next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+// import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
